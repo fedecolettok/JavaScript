ae87551e6d48abc7e58604548922632decf6e885
fedecolettok <fedecolettok@gmail.com>
2023-02-28 20:18:37 -0300

    hasta copyWithin

--- javascript.js
+++ javascript.js
@@ -17,6 +17,8 @@ var fruits = ["Banana", "Orange", "Apple", "Mango"];
 console.log(fruits.constructor);
 
 // copyWithin()
-// Copiar los 2 primeros elementos del arra
-var fruits = ["Banana", "Orange", "Apple", "Mango"];
-console.log(fruits.copyWithin(2, 0, 2));
+var nums = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+// Copia los elementos desde la posición 0 (start) hasta la posición 3 (end) ['a', 'b', 'c'] en la posición 2 (target) del arreglo.
+console.log(nums.copyWithin(2, 0, 3));
+// Copia los elementos desde la posición 2 (start) hasta la posición 5 (end) ['c', 'd', 'e'] en la posición 0 (target) del arreglo.
+console.log(nums.copyWithin(0, 3, 5));
